ab6ef43bc6cb7dcfe25207c561ee01b2e4d5d011
Vladyslav Ogol <vladyslav.ogol@onseostudio.com>
2022-11-02 13:07:03 +0200

    Initial commit

---
+++ src/main/java/springdoc/bug/SpringdocJavadocMultipartBugApplication.java
@@ -0,0 +1,13 @@
+package springdoc.bug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocJavadocMultipartBugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdocJavadocMultipartBugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springdoc/bug/SpringdocJavadocMultipartBugApplicationTests.java
@@ -0,0 +1,13 @@
+package springdoc.bug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdocJavadocMultipartBugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
